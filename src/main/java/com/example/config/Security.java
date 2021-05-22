ff4945632d73fe3a1698a6715b5fe040e549dbd6
masuda220 <masuda220@gmail.com>
2021-05-23 08:12:26 +0900

    API方式に変更;ログアウトの実装

--- src/main/java/com/example/config/Security.java
+++ src/main/java/com/example/config/Security.java
@@ -3,27 +3,30 @@ package com.example.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-@EnableWebSecurity
 public class Security extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(urlRegistry -> urlRegistry
-                        .antMatchers("/", "/error").permitAll()
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout(logoutConfigurer -> logoutConfigurer
                         .logoutSuccessUrl("/").permitAll()
                 )
-                // 401 Unauthorized を返す
-//                .exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer
-//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//                )
+              // POSTのCSRF対策
+              .csrf(c -> c
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+              )
+              // 認証エラー時の例外
+                .exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
                 .oauth2Login();
     }
 }
